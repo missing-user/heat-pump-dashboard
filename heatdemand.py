9ef9d8e456aff9ea702d4ed6542fc8f429380191
Max Ebhart <st161290@stud.uni-stuttgart.de>
2023-09-22 16:42:08 +0200

    q_H

---
+++ heatdemand.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+tab_heat_demand = pd.read_csv("data/heatingload/room_heating/spezifische Heizlast.csv",sep= ";" )
+
+def heat_demand(df, b_type, b_age, t_design):
+  q_H_design = tab_heat_demand.loc[tab_heat_demand["building_type"]== b_type, b_age].iloc[0]
+  t_lim = tab_heat_demand.loc[tab_heat_demand["building_type"]== "Heizgrenztemperatur", b_age].iloc[0]
+  m = -q_H_design/(abs(t_design)+t_lim)
+  c = q_H_design - abs(t_design) * (q_H_design/(abs(t_design)+t_lim))
+  t_amb = df["temp"]
+  q_H = m*t_amb+c
+  df["q_H"]=q_H
+  return df
+print(heat_demand(0,"single_family_house", "1969-73",0))
