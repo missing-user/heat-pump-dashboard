52d0aa75af32b562d46052009f2157033829cabe
Philipp Jurasic <philipp.jurasic@tum.de>
2023-09-22 17:58:24 +0200

    no Date printing

--- co2intensity.py
+++ co2intensity.py
@@ -14,11 +14,9 @@ def add_intensity_column(power_df, intensity_df):
     if (intensity_df["Emissions [g CO2eq/kWh]"] == energy_type).any():  
       intensity_name = energy_type.replace("[MWh] Calculated resolutions", "[g CO2eq/kWh]")
       power_df[intensity_name] = power_df[energy_type] * intensity_lookup.loc[energy_type, "Med"]
-    else:
-      print(energy_type)
 
   power_df["CO2 sum"] = sum([power_df[col] for col in power_df.columns if "[g CO2eq/kWh]" in col])
-  power_df["kWh sum"] = sum([power_df[col] for col in power_df.columns if "[MWh] Calculated resolutions" in col])
+  power_df["kWh sum"] = sum([1e3*power_df[col] for col in power_df.columns if "[MWh] Calculated resolutions" in col])
   power_df["Intensity"]=power_df["CO2 sum"] / power_df["kWh sum"]
   return power_df.set_index("Date")
 
@@ -34,6 +32,5 @@ if __name__ == "__main__":
   power_df = load_all()
   # power_df["Intensity"] is the column of interest
 
-  px.line(power_df, x="Date",y="Intensity").show()
-  px.area(power_df, x="Date", 
-    y=[col for col in power_df.columns if "[g CO2eq/kWh]" in col]).show()
+  px.line(power_df, y="Intensity").show()
+  px.area(power_df, y=[col for col in power_df.columns if "[g CO2eq/kWh]" in col]).show()
