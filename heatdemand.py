ed30e623953ad6aa99e92edf13af425395ea7b1f
Julius Neszvecsko <julius.neszvecsko@tum.de>
2023-09-27 18:36:04 +0200

    new heat pumps

--- heatdemand.py
+++ heatdemand.py
@@ -1,111 +1,132 @@
 import pandas as pd
 import numpy as np
+import heatings
 
-tab_heat_demand = pd.read_csv("data/heatingload/room_heating/spezifische Heizlast.csv",sep= ";" )
-uwerte = pd.read_csv("data/heatingload/room_heating/Uwerte.csv",sep=";").set_index("building_year")
-gwerte = pd.read_csv("data/heatingload/room_heating/Gwerte.csv").set_index("Gebaeude Alter")
-cwerte = pd.read_csv("data/heatingload/room_heating/Cwerte.csv").set_index("Gebaeude Alter")
+tab_heat_demand = pd.read_csv("data/heatingload/room_heating/spezifische Heizlast.csv", sep=";")
+uwerte = pd.read_csv("data/heatingload/room_heating/Uwerte.csv").set_index("building_year")
+gwerte = pd.read_csv("data/heatingload/room_heating/Gwerte.csv").set_index("building_year")
+cwerte = pd.read_csv("data/heatingload/room_heating/Cwerte.csv").set_index("building_year")
+
 
 def heat_pump_size(b_type, b_age, A):
-  q_dot_H_design = tab_heat_demand.loc[tab_heat_demand["building_type"]== b_type, b_age].iloc[0] * 1e-3 #W to kW
-  Q_dot_H_design = q_dot_H_design * A
-  return Q_dot_H_design
+    q_dot_H_design = tab_heat_demand.loc[tab_heat_demand["building_type"] == b_type, b_age].iloc[0] * 1e-3  # W to kW
+    Q_dot_H_design = q_dot_H_design * A
+    return Q_dot_H_design
 
 
 def get_heatpump_Q_dot(t_current, t_target, Q_dot_H_design):
-   if t_current <= t_target:
-      return Q_dot_H_design
-   return 0.0
+    if t_current <= t_target:
+        return Q_dot_H_design
+    return 0.0
 
 
-def simulate_np(P_internal:np.ndarray, T_outside_series:np.ndarray,
-                ventilation:np.ndarray, intensity_series:np.ndarray,
-                Q_dot_H_design:float, t_target:float, 
-                UA:float, C:float):
-  timestep = 3600.0 # h
+def simulate_np(P_internal: np.ndarray, T_outside_series: np.ndarray,
+                ventilation: np.ndarray, intensity_series: np.ndarray, Q_dot_supplied: np.ndarray,
+                Q_dot_H_design: float, t_target: float,
+                UA: float, C: float):
+    timestep = 3600.0  # h
+    t_range = 2.
 
-  Q_dot_loss = np.zeros_like(T_outside_series)
-  Q_dot_ventilation = np.zeros_like(T_outside_series)
-  Q_dot_H = np.zeros_like(T_outside_series)
-  Q_H = np.zeros_like(T_outside_series)
-  Q_H[0] = t_target*C # initial temperature
+    Q_dot_loss = np.zeros_like(T_outside_series)
+    Q_dot_ventilation = np.zeros_like(T_outside_series)
+    Q_dot_required = np.zeros_like(T_outside_series)
+    Q_H = np.zeros_like(T_outside_series)
+    Q_H[0] = t_target * C  # initial temperature
 
-  for i in range(len(T_outside_series)-1):
-      T_inside = Q_H[i]/C
-      T_outside = T_outside_series[i]
-      
-      Q_dot = Q_dot_loss[i] = (T_outside - T_inside)*UA
-      
-      Q_dot_ventilation[i] = (T_outside - T_inside)*ventilation[i]
-      Q_dot += Q_dot_ventilation[i] # ventilation (already has a negative sign)
-      
-      Q_dot += P_internal[i] # appliances
+    for i in range(len(T_outside_series) - 1):
+        T_inside = Q_H[i] / C
+        T_outside = T_outside_series[i]
 
-      if T_inside <= t_target:
+        Q_dot = Q_dot_loss[i] = (T_outside - T_inside) * UA
+        Q_dot_ventilation[i] = (T_outside - T_inside) * ventilation[i]
+        Q_dot += Q_dot_ventilation[i]  # ventilation (already has a negative sign)
+
+        Q_dot += P_internal[i]  # appliances
+
+        # building already warm enough -> no heating supplied
+        #if T_inside > t_target+t_range:
+            #Q_dot_supplied[i] = 0.
+
         if intensity_series[i] < 500:
-          pass  #TODO: dynamic threshold for optimal CO2 usage
-        Q_dot_H[i] = max(0,min(-Q_dot, Q_dot_H_design))
+            if T_inside > t_target+t_range:
+                # TODO: dynamic threshold for optimal CO2 usage
+                Q_dot_supplied[i] = 0.
+        else:
+            if T_inside > t_target - t_range:
+                Q_dot_supplied[i] = 0.
 
-      Q_dot += Q_dot_H[i] # heat pump
-      Q_H[i+1] = Q_H[i] + Q_dot*timestep
-  return Q_H, Q_dot_loss, Q_dot_ventilation, Q_dot_H
+        Q_dot_required[i] = max(0, -Q_dot)
+        Q_dot += Q_dot_supplied[i]  # heat pump
+        Q_H[i + 1] = Q_H[i] + Q_dot * timestep
+    return Q_H, Q_dot_loss, Q_dot_ventilation, Q_dot_required, Q_dot_supplied
+
 
 def ventilation(b_type, volume):
-  c_spec_air = 1.006 # kJ/kgK
-  air_density = 1.2 # kg/m3
-  c_air = c_spec_air * air_density # kJ/m3K
+    c_spec_air = 1.006  # kJ/kgK
+    air_density = 1.2  # kg/m3
+    c_air = c_spec_air * air_density  # kJ/m3K
 
-  return 0.5 * volume * c_air / 3600.0 # kJ/sK
+    return 0.5 * volume * c_air / 3600.0  # kJ/sK
+
 
 def calc_U(b_age, A_windows, A, n_floors, h_floor=3):
-    basement = A/n_floors
-    roof= A/n_floors
-    A_outsidewall = np.square(A/n_floors)*h_floor*n_floors*4
+    A_basement = A / n_floors
+    A_roof = A / n_floors
+    A_outsidewall = np.sqrt(A / n_floors) * h_floor * n_floors * 4
 
-    if(b_age=="KfW 70" or b_age=="KfW 40"):
-        U = (A_outsidewall + basement + roof) * uwerte.loc[b_age, "overall [W/m2K]"]  # W/K
+    if (b_age == "KfW 70" or b_age == "KfW 40"):
+        UA = (A_outsidewall + A_basement + A_roof) * uwerte.loc[b_age, "overall [kW/m2K]"]  # W/K
     else:
-        U = (((A_outsidewall-A_windows) * uwerte.loc[b_age,"outside wall [W/m2K]"])+
-             (roof * uwerte.loc[b_age,"roof [W/m2K]"])+
-             (basement* uwerte.loc[b_age,"basement [W/m2K]"])+
-             (A_windows* uwerte.loc[b_age,"windows [W/m2K]"])) #W/K
+        UA = (((A_outsidewall - A_windows) * uwerte.loc[b_age, "outside wall [kW/m2K]"]) +
+             (A_roof * uwerte.loc[b_age, "roof [kW/m2K]"]) +
+             (A_basement * uwerte.loc[b_age, "basement [kW/m2K]"]) +
+             (A_windows * uwerte.loc[b_age, "windows [kW/m2K]"]))  # W/K
 
-    return U
+    UA *= 1e-3
+    return UA
 
-def simulate(df, b_type, b_age, A, A_windows, n_floors=2, t_target=20.0, assumptions=[]):
-  Q_dot_H_design = heat_pump_size(b_type, b_age, A)
-  U = calc_U(b_age, A_windows, A, n_floors)
-  specific_heat_capa = cwerte.loc[b_age, "Heatcapacity [kJ/m3K]"]
 
-  volume = A * 3.0 # m3
-  #specific_heat_capa = 546.66 # Ullis Wert kJ/m3K
-  C = volume*specific_heat_capa    # kJ/K
+def simulate(df, hp_type, b_type, b_age, A, A_windows, n_floors=2, t_target=20.0, assumptions=[]):
+    Q_dot_H_design = heat_pump_size(b_type, b_age, A)
+    UA = calc_U(b_age, A_windows, A, n_floors)
+    specific_heat_capa = cwerte.loc[b_age, "Heatcapacity [kJ/m3K]"]
 
-  # Heat transfer coefficient through walls etc.
-  UA = A*U # UA = 582e-3   # kW/K
-  
-  if "Ventilation heat losses" in assumptions:
-    ventilation_series = np.full_like(df["T_outside [°C]"], ventilation(b_type, volume))
-  else:
-    ventilation_series = np.zeros_like(df["T_outside [°C]"])
+    volume = A * 3.0  # m3
+    # specific_heat_capa = 546.66 # Ullis Wert kJ/m3K
+    C = volume * specific_heat_capa  # kJ/K
 
-  df["P_solar [kW]"] = float(A_windows)/4 *(df["p_solar south [kW/m2]"]+df["p_solar east [kW/m2]"]+df["p_solar west [kW/m2]"])
-  df["Q_dot_solar [kW]"] = df["P_solar [kW]"] * gwerte.loc[b_age, "G-Wert [-]"] # Less heat passes through newer windows
-  
-  # Simulate closing the blinds when it is hot outside
-  if "Close window blinds in summer" in assumptions:
-    df.loc[df["T_outside [°C]"] > t_target,"Q_dot_solar [kW]"] *= 0.1
+    if "Ventilation heat losses" in assumptions:
+        ventilation_series = np.full_like(df["T_outside [°C]"], ventilation(b_type, volume))
+    else:
+        ventilation_series = np.zeros_like(df["T_outside [°C]"])
 
-  P_internal = (df["P_el appliances [kW]"] + df["Q_dot_solar [kW]"]).to_numpy()
+    df["P_solar [kW]"] = float(A_windows) / 4 * (
+                df["p_solar south [kW/m2]"] + df["p_solar east [kW/m2]"] + df["p_solar west [kW/m2]"])
+    df["Q_dot_solar [kW]"] = df["P_solar [kW]"] * gwerte.loc[
+        b_age, "G-Wert [-]"]  # Less heat passes through newer windows
 
-  Q_H, Q_dot_loss, Q_dot_vent, Q_dot_H = simulate_np(P_internal, 
-                                         df["T_outside [°C]"].to_numpy(),
-                                         ventilation_series, 
-                                         Q_dot_H_design, t_target, UA, C)
-  df["Q_H [kJ]"] = Q_H
-  df["Q_dot_loss [kW]"] = Q_dot_loss
-  df["Q_dot_ventilation [kW]"] = Q_dot_vent
-  df["Q_dot_H [kW]"] = Q_dot_H
+    # Simulate closing the blinds when it is hot outside
+    if "Close window blinds in summer" in assumptions:
+        df.loc[df["T_outside [°C]"] > t_target, "Q_dot_solar [kW]"] *= 0.1
 
-  df["T_house [°C]"] = df["Q_H [kJ]"]/C # kJ to °C
-  return df
+    P_internal = (df["P_el appliances [kW]"] + df["Q_dot_solar [kW]"]).to_numpy()
+    df = heatings.simulate_hp(df, model=hp_type)
+
+    Q_H, Q_dot_loss, Q_dot_vent, Q_dot_required, Q_dot_supplied = simulate_np(P_internal,
+                                                                              df["T_outside [°C]"].to_numpy(),
+                                                                              ventilation_series,
+                                                                              df["Intensity [g CO2eq/kWh]"].to_numpy(),
+                                                                              df['Q_dot_supplied [kW]'].to_numpy(),
+                                                                              Q_dot_H_design, t_target, UA, C)
+    df["Q_H [kJ]"] = Q_H
+    df["Q_dot_loss [kW]"] = Q_dot_loss
+    df["Q_dot_ventilation [kW]"] = Q_dot_vent
+    df["Q_dot_required [kW]"] = Q_dot_required
+    df['Q_dot_supplied [kW]'] = Q_dot_supplied
+
+    df.loc[df.loc[:, 'Q_dot_supplied [kW]'] == 0, 'COP heatpump'] = np.nan
+    df.loc[df.loc[:, 'Q_dot_supplied [kW]'] == 0, 'P_el heat pump [kW]'] = 0.
+    df["heat pump emissions [kg CO2eq]"] = df["P_el heat pump [kW]"] * df["Intensity [g CO2eq/kWh]"] * 1e-3
+
+    df["T_house [°C]"] = df["Q_H [kJ]"] / C  # kJ to °C
+    return df
--- heatings.py
+++ heatings.py
@@ -1,16 +1,30 @@
 import numpy as np
+import pandas as pd
+import hplib as hpl
+import hplib_database as hpd
+import time
+from scipy.interpolate import interp1d
+
+vorlauf_lookup  = {'t_amb': np.array([-15, -10, 0, 10, 25, 26, 50]),
+                   't_vl': np.array([90, 80, 70, 70, 60, 0, 0]),
+                   't_rl': np.array([60, 60, 55, 55, 55, 0, 0])}
 
 def compute_cop(df,model, t_vl=35.):
     df.loc[:, 'COP heatpump'] = np.nan
     if model == 'Carnot':
         df = cop_carnot(df, t_vl)
+        #df = compute_P_electrical(df)
     elif model == 'soph':
         df.loc[:, 'COP heatpump'] = 1
+        #df = compute_P_electrical(df)
+    else:
+        df = simulate_hp(df)
     return df
 
-def cop_carnot(df, t_vl, degradation_coeff=.5):
-    df.loc[:, 'COP heatpump'] = degradation_coeff * (273.15 + t_vl) / (t_vl - df.loc[:, 'T_outside [°C]'])
-    return df
+def cop_carnot(df, degradation_coeff=.5):
+    result_df = pd.DataFrame()
+    result_df['COP heatpump'] = degradation_coeff * (273.15 + df.loc[:,'T_vorlauf [°C]']) / (df.loc[:,'T_vorlauf [°C]'] - df.loc[:, 'T_outside [°C]'])
+    return result_df
 
 def compute_P_electrical(df):
     df.loc[:, 'P_el heat pump [kW]'] = df.loc[:, 'Q_dot_H [kW]'] / df.loc[:, 'COP heatpump']
@@ -24,7 +38,7 @@ def gas_heating(df):
     eta = .95
 
     # compute power demand and emissions
-    df.loc[:, 'Gas heating emissions [kg CO2eq]'] = df.loc[:,'Q_dot_H [kW]'] / eta * intensity * 1e-3
+    df.loc[:, 'Gas heating emissions [kg CO2eq]'] = df.loc[:,'Q_dot_required [kW]'] / eta * intensity * 1e-3
     return df
 
 def oil_heating(df):
@@ -33,5 +47,80 @@ def oil_heating(df):
     eta = .95
 
     # compute power demand and emissions
-    df.loc[:, 'Oil heating emissions [kg CO2eq]'] = df.loc[:,'Q_dot_H [kW]'] / eta * intensity * 1e-3
+    df.loc[:, 'Oil heating emissions [kg CO2eq]'] = df.loc[:,'Q_dot_required [kW]'] / eta * intensity * 1e-3
     return df
+
+def simulate_hp_inverse(df, model='Bosch Compress 3000 AWS-8 B'):
+    # Create heat pump object with parameters
+    parameters = hpl.get_parameters(model=model)
+    heatpump = hpl.HeatPump(parameters)
+
+    # generate lookup table for heatpump library
+    t_amb_min = df['t_outside [°C]'].min()
+    t_amb_max = df['t_outside [°C]'].max()
+    t_amb = np.arange(t_amb_min,t_amb_max+1.)
+    t_amb = np.unique(df['T_outside [°C]'].values)
+    t_amb.sort()
+    t_in_secondary = np.arange(25,85)
+    t_in_secondary_length = len(t_in_secondary)
+    t_ambs = []
+    t_secondarys = []
+    for t in t_amb:
+        t_ambs.extend([t]*t_in_secondary_length)
+        t_secondarys.extend(t_in_secondary)
+    df_lookup = pd.DataFrame({'T_amb': t_ambs, 'T_in_secondary': t_secondarys})
+
+    # calculate/simulate with values from lookuptable
+    start = time.time()
+    results = heatpump.simulate(t_in_primary=df_lookup['T_amb'].values, t_in_secondary=df_lookup['T_in_secondary'].values, t_amb=df_lookup['T_amb'].values, mode=1)
+    # TODO:
+    #  pick best t_vl/P_el/Q_h
+    #  add values to DataFrame
+    results_lookup = pd.DataFrame(results)
+    end = time.time()
+    calc_time = end - start
+    results_lookup.loc[:,'COP Carnot'] = .4 * (273.15 + results_lookup['T_out']) / (results_lookup['T_out'] - results_lookup['T_in'])
+    results_lookup.loc[:, 'P_th'] = results_lookup.loc[:, 'P_th'] * 1e-3
+    results_lookup.loc[:, 'P_el'] = results_lookup.loc[:, 'P_el'] * 1e-3
+
+    for iter,row in df[['Q_dot_H [kW]','T_outside [°C]']].iterrows():
+        temp_df = results_lookup[results_lookup['T_amb'] == row['T_outside [°C]']]
+        interpolation_function = interp1d(temp_df['P_th'].values, temp_df['COP'].values, kind='linear')
+        new_COP = interpolation_function(row['Q_dot_H [kW]'])
+        pass
+
+    df.loc[:,'P_el heat pump [kW]'] = np.nan
+    df.loc[:,'Q_dot_heatpump [kW]'] = np.nan
+    df.loc[:, 'COP heatpump'] = np.nan
+    return df
+
+def simulate_hp(df,model):
+    # Create heat pump object with parameters
+    parameters = hpl.get_parameters(model=model)
+    heatpump = hpl.HeatPump(parameters)
+
+    vorlauf_interpfunc = interp1d(vorlauf_lookup['t_amb'], vorlauf_lookup['t_vl'], kind='linear')
+    df['T_vorlauf [°C]'] = vorlauf_interpfunc(df['T_outside [°C]'])
+    df['T_in_secondary'] = df['T_vorlauf [°C]'] - 5.
+
+    if model != 'Carnot':
+        results = heatpump.simulate(t_in_primary=df['T_outside [°C]'].values,
+                                t_in_secondary=df['T_in_secondary'].values, t_amb=df['T_outside [°C]'   ].values,
+                                mode=1)
+        results_df = pd.DataFrame(results)
+        df.loc[:,'COP heatpump'] = results_df['COP'].values
+        df.loc[:,'P_el heat pump [kW]'] = results_df['P_el'].values * 1e-3
+        df.loc[:,'Q_dot_supplied [kW]'] = results_df['P_th'].values * 1e-3
+    else:
+        results_df = cop_carnot(df,degradation_coeff=.4)
+        df['COP heatpump'] = results_df['COP heatpump']
+        df['P_el heat pump [kW]'] = 3
+        df['Q_dot_supplied [kW]'] = 9
+
+    return df
+
+
+
+
+if __name__ == '__main__':
+    simulate_hp(pd.read_csv('test_data.csv'),model='Bosch Compress 3000 AWS-8 B')
